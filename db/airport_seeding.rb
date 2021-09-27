824a37d07ff5ab0533480bcd4ae957bcb89e508c
qpongratz <lagarathan@gmail.com>
2021-09-27 17:28:56 -0500

    Clean up airport_seeding

--- db/airport_seeding.rb
+++ db/airport_seeding.rb
@@ -4,9 +4,7 @@ def seed_airports
   CSV.foreach(Rails.root.join('db/data/airports.csv'), headers: true) do |row|
     next unless row['type'] == 'large_airport' && row['iso_country'] == 'US'
 
-    Airport.create({
-      name: row['name'],
-      code: row['local_code']
-    })
+    Airport.create({ name: row['name'],
+                     code: row['local_code'] })
   end
 end
