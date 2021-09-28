479e7d9f7440fe1dea036ac8e55d87a7c4bc6ab8
qpongratz <lagarathan@gmail.com>
2021-09-28 12:24:04 -0500

    Build basic search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,8 @@
 class FlightsController < ApplicationController
   def index
+    @airport_options = Airport.all.map{ |a| [ a.code, a.id ] }
     @flights = Flight.all.includes(:origin, :destination)
+    @date_options = @flights.map{ |d| [d.departure_date_formatted, d.departure_date_formatted] }.uniq
+    @selected_flights
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :origin, class_name: 'Airport'
   belongs_to :destination, class_name: 'Airport'
+
+  def departure_date_formatted
+    self.departure_time.strftime("%m/%d/%Y")
+  end
 end
