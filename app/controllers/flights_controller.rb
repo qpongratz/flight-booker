e9a0187820349dab87c150fc91893049735c9cff
qpongratz <lagarathan@gmail.com>
2021-09-28 17:48:01 -0500

    Get selected flights working with simple forms

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,16 +4,16 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @flight = Flight.first
     @date_options = @flights.map { |d| [d.departure_date, d.departure_date] }.uniq
-    # return unless params[:commit] == 'Find Flights'
+    return unless params[:commit] == 'Find Flights'
 
-    # @selected_flights = Flight.all.includes(:origin, :destination)
-    #                           .path(flight_params[:origin], flight_params[:destination])
-    #                           .where(departure_date: flight_params[:departure_date])
+    @selected_flights = Flight.all.includes(:origin, :destination)
+                              .path(flight_params[:origin_id], flight_params[:destination_id])
+                              .where(departure_date: flight_params[:departure_date])
   end
 
   private
 
-  # def flight_params
-  #   params.require(:flights).permit(:origin, :destination, :departure_date, :number_of_tickets)
-  # end
+  def flight_params
+    params.require(:flight).permit(:origin_id, :destination_id, :departure_date, :number_of_tickets)
+  end
 end
