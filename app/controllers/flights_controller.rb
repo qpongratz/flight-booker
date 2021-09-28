4f709d9fdc46971339790585ca9b4d0ba7de537e
qpongratz <lagarathan@gmail.com>
2021-09-28 17:43:32 -0500

    Redo first form with simple form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,11 +2,18 @@ class FlightsController < ApplicationController
   def index
     @airport_options = Airport.all.map { |a| [a.code, a.id] }
     @flights = Flight.all
+    @flight = Flight.first
     @date_options = @flights.map { |d| [d.departure_date, d.departure_date] }.uniq
-    return unless params[:commit] == 'Find Flights'
+    # return unless params[:commit] == 'Find Flights'
 
-    @selected_flights = Flight.all.includes(:origin, :destination)
-                              .path(params[:origin_id], params[:destination_id])
-                              .where(departure_date: params[:departure_date])
+    # @selected_flights = Flight.all.includes(:origin, :destination)
+    #                           .path(flight_params[:origin], flight_params[:destination])
+    #                           .where(departure_date: flight_params[:departure_date])
   end
+
+  private
+
+  # def flight_params
+  #   params.require(:flights).permit(:origin, :destination, :departure_date, :number_of_tickets)
+  # end
 end
