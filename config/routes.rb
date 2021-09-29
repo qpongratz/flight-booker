380ecd794555be727278bbe16ceb771c62f0f4d9
qpongratz <lagarathan@gmail.com>
2021-09-28 20:45:22 -0500

    Flight booking form filled out with details

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flights, only: %i[index]
+  resources :bookings, only: %i[new]
   root 'flights#index'
 end
