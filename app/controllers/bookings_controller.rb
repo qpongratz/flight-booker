80b400399b04ccd7f14512b9d426bdebcf94c6df
qpongratz <lagarathan@gmail.com>
2021-09-29 11:45:22 -0500

    Confirm booking saves passengers now

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,15 +1,31 @@
 class BookingsController < ApplicationController
   def new
     @booking = Booking.new(booking_params)
-    @passengers = []
-    @booking.number_of_passengers.times do
-      @passengers << @booking.passengers.build
+    booking_params[:number_of_passengers].to_i.times do
+      @booking.passengers.build
     end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking
+    else
+      render :new
+    end
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+    @passengers = @booking.passengers.all
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:flight_id, :number_of_passengers)
+    params.require(:booking).permit(:flight_id,
+                                    :number_of_passengers,
+                                    passengers_attributes: %i[name email booking_id id])
   end
 end
