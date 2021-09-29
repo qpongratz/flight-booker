183f6afb5a23e8c65b659baa8b43e6b55d57f093
qpongratz <lagarathan@gmail.com>
2021-09-29 09:58:27 -0500

    Clean up code

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,11 +3,7 @@ class Flight < ApplicationRecord
   belongs_to :destination, class_name: 'Airport'
   has_many :bookings, inverse_of: :flight
   # potentially has_many :passengers through bookings
-
-  accepts_nested_attributes_for :bookings
-
   scope :path, ->(origin, destination) { where(origin_id: origin, destination_id: destination)}
-
   def departure_date_formatted
     departure_time.strftime('%Y-%m-%d')
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :flights, only: %i[index]
   resources :bookings, only: %i[new create show]
-  resources :passengers, only: %i[new create show]
   root 'flights#index'
 end
