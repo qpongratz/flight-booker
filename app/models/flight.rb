ca325d3d3ebe51500d5e974917c5e6d54601a1d8
qpongratz <lagarathan@gmail.com>
2021-09-28 21:42:22 -0500

    Update routes and comments

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,7 @@ class Flight < ApplicationRecord
   belongs_to :origin, class_name: 'Airport'
   belongs_to :destination, class_name: 'Airport'
   has_many :bookings, inverse_of: :flight
+  # potentially has_many :passengers through bookings
 
   accepts_nested_attributes_for :bookings
 
