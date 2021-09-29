015369661fbe5c7d462c3fcf85fdc79ad0634430
qpongratz <lagarathan@gmail.com>
2021-09-28 21:38:53 -0500

    Add passenger model and link with booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_225547) do
+ActiveRecord::Schema.define(version: 2021_09_29_023744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,13 @@ ActiveRecord::Schema.define(version: 2021_09_28_225547) do
     t.date "departure_date"
     t.index ["destination_id"], name: "index_flights_on_destination_id"
     t.index ["origin_id"], name: "index_flights_on_origin_id"
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "flights", "airports", column: "destination_id"
